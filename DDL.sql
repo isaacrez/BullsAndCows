bd8618dd1842d71e8ec4a093ffba71aa4bfcd5b4
Isaac Rezey <isaacrez20@gmail.com>
2020-12-17 10:58:16 -0500

    Initial commit

---
+++ DDL.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS bullsAndCowsDB;
+CREATE DATABASE bullsAndCowsDB;
+
+USE bullsAndCowsDB;
+
+CREATE TABLE Games (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    finished BOOL DEFAULT false,
+    answer CHAR(4) NOT NULL
+);
+
+CREATE TABLE Round (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    gameId INT,
+    FOREIGN KEY fk_Game (gameId)
+		REFERENCES Games(id),
+	
+    guess CHAR(4) NOT NULL,
+    `time` DATETIME NOT NULL DEFAULT  NOW()
+);
